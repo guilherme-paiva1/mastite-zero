9b94ca59bb499f9b4073f4bf9115eba5cbfab1f2
l-furquim <lucas.furquim@sptech.school>
2024-10-05 09:30:33 -0300

    Alteração no estilo da calculadora e na validação de entrada do cadastro

--- site-institucional/models/cadastro.js
+++ site-institucional/models/cadastro.js
@@ -13,16 +13,32 @@ function cadastrar(){
     spanErroCnpj.innerHTML = "";
     spanErroEmpresa.innerHTML = "";
     divMensagem.innerHTML = "";
+    inputEmpresa.style = "";
+    inputResponsavel.style = "";
+    inputCnpj.style = "";
+    inputEmail.style = "";
+    inputSenha.style = "";
+    inputConfirmaSenha.style = "";
+
+
 
 
     if(senha != confirmarSenha){
         spanErroSenha.innerHTML = "As senhas não são identicas"
         spanErroConfirmarSenha.innerHTML = "As senhas não são identicas"
+        inputSenha.style.border = "solid 1px red";
+        inputConfirmaSenha.style.border = "solid 1px red";
+
     }else if(senha == 0 || senha < 4){
         spanErroSenha.innerHTML = "Senha inválida, curta demais"
         spanErroConfirmarSenha.innerHTML = "Senha inválida, curta demais"
-    }else if(email == 0 || email < 7){
+        inputSenha.style.border = "solid 1px red";
+        inputConfirmaSenha.style.border = "solid 1px red";
+
+    }else if((email == 0 || email < 7) || email.indexOf("@") < 0 || email.indexOf(".com") < 0){
+        inputEmail.style.border = "solid 1px red";
         spanErroEmail.innerHTML = "Email inválido"
+
     }else if(responsavel == 0 || responsavel < 4){
         spanErroResponsavel.innerHTML = "Responsável inválido"
     }else if(empresa == 0){
@@ -30,4 +46,23 @@ function cadastrar(){
     }else {
         divMensagem.innerHTML = `BEM VINDO AMIGO ${empresa}`
     }
+}
+
+function validar(){
+
+    var empresa =  inputEmpresa.value 
+    var responsavel = inputResponsavel.value 
+    var cnpj = inputCnpj.value 
+    var email = inputEmail.value
+    var senha = inputSenha.value
+    var confirmarSenha = inputConfirmaSenha.value
+
+    console.log(cnpj.length)
+
+    if(cnpj.length < 14 || cnpj.length > 14){
+        inputCnpj.style.border = "solid 1px red";
+    }else{
+        inputCnpj.style.border = "none";
+    }
+
 }
