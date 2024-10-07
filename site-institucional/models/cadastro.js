6793d55ca4e524e97186b81f3db782d5d080eda5
luccaboliveira <lucca.oliveira@sptech.school>
2024-10-06 21:40:49 -0300

    Ajustes no CSS e na validação do cadastro

--- site-institucional/models/cadastro.js
+++ site-institucional/models/cadastro.js
@@ -44,7 +44,7 @@ function cadastrar(){
     }else if(empresa == 0){
         spanErroEmpresa.innerHTML = "Empresa inválida"
     }else {
-        divMensagem.innerHTML = `BEM VINDO AMIGO ${empresa}`
+        divMensagem.innerHTML = `Cadastro realizado com sucesso! Bem-vindo ${empresa}`
     }
 }
 
@@ -61,8 +61,49 @@ function validar(){
 
     if(cnpj.length < 14 || cnpj.length > 14){
         inputCnpj.style.border = "solid 1px red";
+        spanErroCnpj.innerHTML = "CNPJ inválido! O CNPJ precisa ter 14 dígitos"
     }else{
         inputCnpj.style.border = "none";
+        spanErroCnpj.innerHTML = ""
+
+    }
+
+}
+
+function validarEmail(){
+    var email = inputEmail.value
+
+    if((email == 0 || email < 7) || email.indexOf("@") < 0 || email.indexOf(".com") < 0){
+        inputEmail.style.border = "solid 1px red";
+        spanErroEmail.innerHTML = "Email inválido!";
+    }else{
+        inputEmail.style.border = "";
+        spanErroEmail.innerHTML = "";
+    }
+
+}
+
+function validarSenha(){
+    var senha = inputSenha.value
+    var confirmarSenha = inputConfirmaSenha.value   
+
+    if(senha != confirmarSenha){
+        spanErroSenha.innerHTML = "As senhas não são identicas"
+        spanErroConfirmarSenha.innerHTML = "As senhas não são identicas"
+        inputSenha.style.border = "solid 1px red";
+        inputConfirmaSenha.style.border = "solid 1px red";
+
+    }else if(senha == 0 || senha < 4){
+        spanErroSenha.innerHTML = "Senha inválida, curta demais"
+        spanErroConfirmarSenha.innerHTML = "Senha inválida, curta demais"
+        inputSenha.style.border = "solid 1px red";
+        inputConfirmaSenha.style.border = "solid 1px red";
+
+    } else {
+        inputSenha.style.border = "";
+        inputConfirmaSenha.style.border = "";
+        spanErroSenha.innerHTML = "";
+        spanErroConfirmarSenha.innerHTML = "";
     }
 
 }
