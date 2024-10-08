d9985744d67cc46358c2e8ab47ceaaa2ffe00b9a
eduardoohata <eduardo.reis@sptech.school>
2024-10-08 10:32:19 -0300

    Atualizacao do Banco de Dados e Diagrama DER

--- banco-de-dados/BD-CBSAFE.sql
+++ banco-de-dados/BD-CBSAFE.sql
@@ -2,36 +2,53 @@ CREATE DATABASE cbsafe;
 
 USE cbsafe;
 
--- 1 TABELA DADOS DA EMPRESA
+
+    CREATE TABLE endereco (
+id_endereco INT PRIMARY KEY AUTO_INCREMENT,
+rua VARCHAR(45),
+numero CHAR(9),
+cep CHAR(9),
+bairro VARCHAR(45),
+cidade VARCHAR(45),
+estado VARCHAR (45)
+)AUTO_INCREMENT = 200;
+
+INSERT INTO endereco (id_endereco, rua, numero, cep, bairro, cidade, estado) VALUES
+
+(default, 'Rua Ari Rodrigues', '777', '69900-013', 'São João',  'Rio Branco',  'Acre'),
+(default, 'Rua Manoel Soares Londres', '71', '58010-010', 'Manaira', 'João Pessoa', 'Paraiba'),
+(default, 'Rua Miramar', '87', '59010-015', 'Alecrim', 'Rio Grande do Norte', 'Natal'), 
+(default, 'Rua Libero', '242', '60011-014', 'Tucanos', 'Londrina', 'Paraná');
+
+select * from endereco;
 
 CREATE TABLE empresa (
 	id_empresa INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR (50),
 	cnpj CHAR (18),
-	estado VARCHAR (20),
-	cidade VARCHAR (40),
-	cep CHAR (9),
-	logradouro VARCHAR (50),
-	numero VARCHAR (5)
+	fk_endereco INT,
+    CONSTRAINT fk_Endereco FOREIGN KEY (fk_endereco)
+    REFERENCES endereco(id_endereco)
 ) AUTO_INCREMENT = 10000;
 
 -- INSERIR DADOS NA TABELA EMPRESA
-INSERT INTO empresa (nome, cnpj, estado, cidade, cep, logradouro, numero) VALUES
-	('Grupo gui', '01.200.477/0001-03', 'Acre', 'Rio Branco', '69900-013', 'Rua Ari Rodrigues', '777'),
-	('Grupo LA', '37.570.300/0001-74', 'Paraiba', 'João Pessoa', '58010-010', 'Rua Manoel Soares Londres', '71'),
-	('JohnEnterprises', '20.780.209/0001-20', 'Rio Grande do Norte', 'Natal', '59010-015', 'Rua Miramar', '87'), 
-    ('Grupo Lucca & Lucas', '33.790.210/0001-30', 'Paraná', 'Londrina', '60011-014', 'Rua Libero', '242');
-
--- MOSTRAR O NOME DA EMPRESA EM QUE O ID É 10001
+INSERT INTO empresa (nome, cnpj, fk_endereco) VALUES
+	('Grupo gui', '01.200.477/0001-03', 200),
+	('Grupo LA', '37.570.300/0001-74', 201),
+	('JohnEnterprises', '20.780.209/0001-20', 202), 
+    ('Grupo Lucca & Lucas', '33.790.210/0001-30', 203);
+    
+    -- MOSTRAR O NOME DA EMPRESA EM QUE O ID É 10001
 SELECT nome FROM empresa
 	WHERE id_empresa = 10001;
 
--- MOSTRAR TODOS OS DADOS DA EMPRESA EM QUE O ESTADO É ACRE E A CIDADE É RIO BRANCO
-SELECT * FROM empresa;
-        
--- TABELA 2 - CADASTRO DE USUÁRIOS
 
--- CRIAR TABELA USUARIO
+SELECT * FROM empresa;
+
+SELECT emp.nome, ende.rua FROM Empresa AS emp JOIN Endereco AS ende ON id_endereco = fk_endereco;
+
+
+
 CREATE TABLE usuario (
 	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
     supervisor VARCHAR (20),
@@ -67,8 +84,7 @@ FROM empresa
 JOIN usuario 
     ON id_empresa = fk_empresa;
     
-    
--- TABELA 3 - COMPOST BARN
+    -- TABELA 3 - COMPOST BARN
 
 -- CRIAR TABELA Compost_barn
 CREATE TABLE compost_barn (
@@ -84,11 +100,11 @@ CREATE TABLE compost_barn (
 ) AUTO_INCREMENT = 1000;
 
 -- INSERIR VALORES NA TABELA
-INSERT INTO compost_barn (area_m2, data_ultima_manutencao, fk_empresa) VALUES
-	('1500', '2024-05-20', 10001),
-	('1000', '2024-09-02', 10002),
-	('1200', '2024-02-17', 10003),
-    ('1100', '2024-03-15', 10000);
+INSERT INTO compost_barn (area_m2, qtd_vacas, data_ultima_manutencao, fk_empresa) VALUES
+	('1500',100,'2024-05-20', 10001),
+	('1000',90,'2024-09-02', 10002),
+	('1200',80,'2024-02-17', 10003),
+    ('1100', 110, '2024-03-15', 10000);
 
 -- EXIBIR TODOS OS DADOS DA TABELA
 SELECT * FROM compost_barn;
@@ -115,21 +131,33 @@ CREATE TABLE dados_sensor(
 	id_dado INT PRIMARY KEY auto_increment,
 	umidade DECIMAL (4,2),
 	data_hora DATETIME,
+	alerta VARCHAR(20),
     fk_compost_barn INT,
     
     CONSTRAINT fk_compost_barn_dados
 		FOREIGN KEY (fk_compost_barn)
-			REFERENCES compost_barn(id_cb)
+			REFERENCES compost_barn(id_cb),
+    
+    CONSTRAINT chk_alerta
+		CHECK (alerta IN ('verde', 'amarelo', 'vermelho'))
         
 ) AUTO_INCREMENT = 100;
 
 -- INSERIR DADOS NA TABELA
-INSERT INTO dados_sensor (umidade, data_hora, fk_compost_barn) VALUES
-	(70.9, '2024-08-01 16:44:00', 1000),
-	(35.6, '2024-08-09 16:50:09', 1001),
-	(44.8, '2024-08-09 18:00:01', 1002),
-    (25.6, '2024-08-09 18:00:01', 1003);
+INSERT INTO dados_sensor (umidade, data_hora, alerta, fk_compost_barn) VALUES
+	(70.9, '2024-08-01 16:44:00', 'vermelho', 1000),
+	(35.6, '2024-08-09 16:50:09', 'verde', 1001),
+	(44.8, '2024-08-09 18:00:01', 'amarelo', 1002),
+    (25.6, '2024-08-09 18:00:01', 'vermelho', 1003);
 
+-- MOSTRAR OS DADOS DO SENSOR EM QUE O ALERTA É VERDE
+SELECT * FROM dados_sensor
+	WHERE alerta = 'verde';
+
+-- MOSTRAR OS DADOS DO SENSOR EM QUE O ALERTA É VERMELHO
+SELECT * FROM dados_sensor
+	WHERE alerta = 'vermelho';
+        
 -- MOSTRAR UMA MENSAGEM QUE UNE O HORÁRIO, REGISTRO E O ALERTA
 SELECT CONCAT('A leitura de umidade no dia e no horário ', data_hora, ' foi ', umidade, '%, o que gerou um alerta ', alerta) AS mensagem
 	FROM dados_sensor;
@@ -145,16 +173,5 @@ SELECT
 		JOIN compost_barn 
 			ON id_cb = fk_compost_barn;
 
+    
 
-
-
-
-
-
-        
-        
-        
-        
-        
-        
-        
