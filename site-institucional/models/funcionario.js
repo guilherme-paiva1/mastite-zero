92a8d4da03a450b8399d0d82487b714ec6719e53
l-furquim <lucas.furquim@sptech.school>
2024-10-25 20:53:33 -0300

    Prototipagem da futura funcionalidade de alterar os dados ou exluir um funcionario

--- site-institucional/models/funcionario.js
+++ site-institucional/models/funcionario.js
@@ -1,11 +1,13 @@
-function novoFuncionario (){
-  var modal = modalNovoFuncionario;  
-  
-  window.onclick = function(event) {
+var vezesClicadas = 0;
+
+function novoFuncionario() {
+  var modal = modalNovoFuncionario;
+
+  window.onclick = function (event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+      modal.style.display = "none";
     }
-}
+  }
 
   modal.style.display = "flex"
 }
@@ -13,4 +15,35 @@ function fecharModal() {
   var modal = modalNovoFuncionario;
 
   modal.style.display = "none";
+}
+
+function excluirUsuario() {
+  var modal = modalExcluirUsuario;
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  modal.style.display = "flex";
+}
+
+function negarExclusao() {
+  var modal = modalExcluirUsuario;
+
+  modal.style.display = "none";
+}
+
+function editarUsuario() {
+
+
+  if (vezesClicadas == 1) {
+    inputEmailFuncionario.disabled = true;
+    vezesClicadas = 0;
+  } else {
+    vezesClicadas++;
+    inputEmailFuncionario.disabled = false;
+    inputEmailFuncionario.focus();
+  }
 }
