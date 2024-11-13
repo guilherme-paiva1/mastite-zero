eedbd5ade8d6b4570fdcbf39cfd22c4954acca6c
eduardoohata <eduardo.reis@sptech.school>
2024-11-13 13:51:31 -0300

    Adicao Grafico Fazenda

--- site-institucional/public/js/dash-fazenda.js
+++ site-institucional/public/js/dash-fazenda.js
@@ -21,3 +21,47 @@ function mostrarCompost(idCompost) {
         selectGrupo.value = '#';
     }
 }
+
+var graficoBarraFazenda = new Chart(document.getElementById('grafico_fazenda').getContext('2d'), {
+    data: {
+        labels: ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"],
+        datasets: [{
+            type: 'line',
+            label: 'Umidade máxima (60%)',
+            borderColor: '#b41b1b',
+            backgroundColor: '#b41b1b',
+            data: [60, 60, 60, 60, 60, 60, 60]
+        }, {
+            type: 'line',
+            label: 'Umidade mínima (40%)',
+            borderColor: '#b41b1b',
+            backgroundColor: '#b41b1b',
+            data: [40, 40, 40, 40, 40, 40, 40]
+        },{
+            type: 'bar',
+            label: 'Umidade média do CB',
+            borderColor: "#22603A",
+            backgroundColor: "#22603A",
+            data: [53, 57, 49, 43, 51, 59, 65]
+        }],
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Umidade ideal x  Umidade média na semana',
+                color: "#22603A",
+                font: {
+                    size: 24,
+                    weight: 'bold',
+                    family: 'Poppins'
+                },
+                padding: {
+                    top: 10,
+                    bottom: 30
+                },
+                align: 'center',
+            }
+        },
+    }
+})
